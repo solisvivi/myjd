8ed479ba17221ca3a6a9602326b0b2f915b406a3
ruicky620 <ruicky620@126.com>
2020-12-08 22:58:08 +0800

    修改直播红包雨时间

--- jd_live_redrain.js
+++ jd_live_redrain.js
@@ -67,7 +67,7 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
         continue
       }
       let nowTs = new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000
-      // console.log(nowTs, $.startTime, $.endTime)
+      console.log(nowTs, $.startTime, $.endTime)
       if ($.startTime <= nowTs && nowTs < $.endTime) {
         await receiveRedRain();
       } else {
